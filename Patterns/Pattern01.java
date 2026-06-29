3cdd88daa1900434eb7a107a22f63919d4cc5539
bindhu-1212 <bindhupunitha@gmail.com>
2026-06-29 12:37:57 +0530

    Add Pattern01.java for printing character patterns

---
+++ Patterns/Pattern01.java
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n=5;
+for(int i=1;i<=n;i++)
+{   char ch='A'+n-i;
+for(int j=1;j<=i;j++)
+{
+System.out.print(ch);
+ch++;
+}
+System.out.println();
+}
+}
